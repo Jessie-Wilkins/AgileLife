cf60cc1b7efc7cf0a71a5b30a4b77d47a2318350
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2020-06-03 16:58:38 -0500

    Changed story related theories tests to test correctly and failed story manager theory.

--- MyApplication/app/src/test/java/com/actual/myapplication/Theories/StoryManagerTheories.java
+++ MyApplication/app/src/test/java/com/actual/myapplication/Theories/StoryManagerTheories.java
@@ -3,22 +3,42 @@ package com.actual.myapplication.Theories;
 import com.actual.myapplication.Story;
 import com.actual.myapplication.StoryManager;
 import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(JUnitQuickcheck.class)
 public class StoryManagerTheories {
 
-    private StoryManager story_mgr = StoryManager.initiateStoryManager();
+   private StoryManager story_mgr = StoryManager.initiateStoryManager();
 
-    @Property public void testThatStoriesDeletedKeepTheSameIdEveryTime() {
-        int id;
+    @Property public void testThatStoriesDeletedKeepTheSameIdEveryTime(@InRange(minInt = 1, maxInt = 1000000)int amount_of_times_to_generate) {
 
-        story_mgr.addStory();
+        long story_id;
+
+        long deleted_id;
+
+        for(int i = 0; i<amount_of_times_to_generate; i++) {
+            story_mgr.addStory();
+        }
+
         long [] array = story_mgr.getStoriesIds();
+
+        story_id = array[(array.length-1)];
+
+        story_mgr.deleteStory(story_id);
+
+        array = story_mgr.getDeletedStoriesIds();
+
+        deleted_id = array[(array.length-1)];
+
+        assertEquals(story_id, deleted_id);
+
     }
 
 }
--- MyApplication/app/src/test/java/com/actual/myapplication/Theories/StoryTheories.java
+++ MyApplication/app/src/test/java/com/actual/myapplication/Theories/StoryTheories.java
@@ -2,6 +2,7 @@ package com.actual.myapplication.Theories;
 
 import com.actual.myapplication.Story;
 import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.runner.RunWith;
 
@@ -12,12 +13,16 @@ public class StoryTheories {
 
     private ArrayList<Integer> id_array = new ArrayList<>();
 
-    @Property public void testThatStoriesCanGenerateAUniqueIdEveryTime() {
-        Story story = new Story();
+    @Property public void testThatStoriesCanGenerateAUniqueIdEveryTime(@InRange(minInt = 1, maxInt = 9999999)int amount_of_times_to_generate) {
+        Story story;
+        for(int i = 0; i<amount_of_times_to_generate; i++) {
+            story = new Story();
+            id_array.add((int) story.getId());
+        }
+
+        story = new Story();
 
         assert(!id_array.contains(story.getId()));
-
-        id_array.add((int) story.getId());
     }
 
 }
