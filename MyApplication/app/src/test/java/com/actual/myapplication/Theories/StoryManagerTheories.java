729ac4bd6cdb9d25f1c9e35baaf487c3382adab6
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2020-06-22 17:17:51 -0500

    Added property tests for story builders.

--- MyApplication/app/src/test/java/com/actual/myapplication/Theories/StoryManagerTheories.java
+++ MyApplication/app/src/test/java/com/actual/myapplication/Theories/StoryManagerTheories.java
@@ -130,7 +130,7 @@ public class StoryManagerTheories {
         assertEquals(completed_id, uncompleted_id);
     }
 
-    @Property public void testThatCreatingAStoryWithABuilderAllowsStoryToReturnSameAssignedValuesEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points) {
+    @Property public void testThatCreatingAStoryWithABuilderAllowsStoryToReturnSameAssignedTitleEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points) {
 
         StoryBuilder.resetBuilderAsUninitiated();
         StoryBuilder story_builder = StoryBuilder.initiateBuilder();
@@ -151,7 +151,295 @@ public class StoryManagerTheories {
 
         story_manager.addStory(story_builder);
 
+        assertEquals(title, story_manager.getStory(1).getTitle());
+
 
     }
+
+    @Property public void testThatCreatingAStoryWithABuilderAllowsStoryToReturnSameAssignedDescriptionEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        assertEquals(description, story_manager.getStory(1).getDescription());
+
+
+    }
+
+    @Property public void testThatCreatingAStoryWithABuilderAllowsStoryToReturnSameAssignedPointsEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        assertEquals(points, story_manager.getStory(1).getPointsManagement().getPoints());
+
+
+    }
+
+    @Property public void testThatCreatingAStoryWithABuilderAllowsStoryToReturnSameAssignedStatusEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        assertEquals(status, story_manager.getStory(1).getStatus());
+
+
+    }
+
+    @Property public void testThatCreatingAStoryWithABuilderAllowsStoryToReturnSameAssignedSprintTitleEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        assertEquals(sprint_title, story_manager.getStory(1).getSprintTitleAndId());
+
+
+    }
+
+    @Property public void testThatCreatingAStoryWithABuilderAllowsStoryToReturnSameAssignedCompletedPointsEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        assertEquals(completed_points, story_manager.getStory(1).getPointsManagement().getCompletedPoints());
+
+
+    }
+
+    @Property public void testThatEditingAStoryWithABuilderViaOnlyChangedAttributesCorrectlyChangesTheChangedTitleEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points,
+                                                                                                                            String changed_title) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        story_builder.setTitle(changed_title);
+
+        story_manager.editStoryWithOnlyChangedAttributes(1, story_builder);
+
+        assertEquals(changed_title, story_manager.getStory(1).getTitle());
+
+
+    }
+
+    @Property public void testThatEditingAStoryWithABuilderViaOnlyChangedAttributesCorrectlyChangesTheChangedDescriptionEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points,
+                                                                                                                            String changed_description) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        story_builder.setDescription(changed_description);
+
+        story_manager.editStoryWithOnlyChangedAttributes(1, story_builder);
+
+        assertEquals(changed_description, story_manager.getStory(1).getDescription());
+
+
+    }
+
+    @Property public void testThatEditingAStoryWithABuilderViaOnlyChangedAttributesCorrectlyChangesTheChangedPointsEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points,
+                                                                                                                                  int changed_points) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        story_builder.setPoints(changed_points);
+
+        story_manager.editStoryWithOnlyChangedAttributes(1, story_builder);
+
+        assertEquals(changed_points, story_manager.getStory(1).getPointsManagement().getPoints());
+
+
+    }
+
+    @Property public void testThatEditingAStoryWithABuilderViaOnlyChangedAttributesCorrectlyChangesTheChangedStatusEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points,
+                                                                                                                             Story.StoryStatus changed_status) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        story_builder.setStatus(changed_status);
+
+        story_manager.editStoryWithOnlyChangedAttributes(1, story_builder);
+
+        assertEquals(changed_status, story_manager.getStory(1).getStatus());
+
+    }
+
+
+    @Property public void testThatEditingAStoryWithABuilderViaOnlyChangedAttributesCorrectlyChangesTheChangedSprintTitleEveryTime(String title, String description, @InRange(minInt = 1, maxInt = 999999) int points, Story.StoryStatus status, String sprint_title, @InRange(minInt = 1, maxInt = 999999) int completed_points,
+                                                                                                                             String changed_sprint_title) {
+
+        StoryBuilder.resetBuilderAsUninitiated();
+        StoryBuilder story_builder = StoryBuilder.initiateBuilder();
+
+        StoryManager story_manager = StoryManager.initiateStoryManager();
+
+        story_builder.setTitle(title);
+
+        story_builder.setDescription(description);
+
+        story_builder.setPoints(points);
+
+        story_builder.setStatus(status);
+
+        story_builder.setSprintTitleAndId(sprint_title);
+
+        story_builder.setCompletedPoints(completed_points);
+
+        story_manager.addStory(story_builder);
+
+        story_builder.setSprintTitleAndId(changed_sprint_title);
+
+        story_manager.editStoryWithOnlyChangedAttributes(1, story_builder);
+
+        assertEquals(changed_sprint_title, story_manager.getStory(1).getSprintTitleAndId());
+
+    }
+
+
 
 }
