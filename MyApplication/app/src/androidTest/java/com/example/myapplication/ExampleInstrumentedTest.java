204d8e7278412c785178f0cb0655a60d3e98ede0
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2020-10-19 17:05:28 -0500

    Added editable line for story title and added first UI test.

--- MyApplication/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ MyApplication/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,13 +1,23 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.view.View;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.actual.myapplication.StoryManager;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +27,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    StoryManager storyManager = StoryManager.initiateStoryManager();
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +37,18 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.myapplication", appContext.getPackageName());
     }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void addPointsToNewStory() {
+        onView(withId(R.id.editPoints))
+                .perform(typeText("4"), closeSoftKeyboard());
+        onView(withId(R.id.button)).perform(click());
+
+        assertEquals(4, storyManager.getStory(1).getPointsManagement().getPoints());
+    }
+
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,22 +19,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void addStoryPoints(View view) {
+    public void addStory(View view) {
 
         StoryBuilder storyBuilder = StoryBuilder.initiateBuilder();
 
         EditText editPoints = findViewById(R.id.editPoints);
 
+        EditText editTitle = findViewById(R.id.editTitle);
+
         System.out.println(editPoints.getText().toString());
 
         if(editPoints.getText().toString() != null && !editPoints.getText().toString().isEmpty()) {
             storyBuilder.setPoints(Integer.parseInt(editPoints.getEditableText().toString()));
-            storyManager.addStory(storyBuilder);
-            System.out.println("Added story #"+ storyManager.getStory(storyManager.getStoriesIds()[storyManager.getStoriesIds().length-1]).getId() +" with these points: "+storyManager.getStory(storyManager.getStoriesIds().length).getPointsManagement().getPoints());
         }
 
         else {
             System.out.println("Points were not successfully added");
         }
+
+        if(editTitle.getText().toString() != null && !editTitle.getText().toString().isEmpty()) {
+            storyBuilder.setTitle(editTitle.getText().toString());
+        }
+
+        else {
+            System.out.println("Title was not successfully added");
+        }
+
+        storyManager.addStory(storyBuilder);
+
+        if(storyManager.getStory(storyManager.getStoriesIds()[storyManager.getStoriesIds().length-1])!= null) {
+            System.out.print("Added story #"+ storyManager.getStory(storyManager.getStoriesIds()[storyManager.getStoriesIds().length-1]).getId());
+        }
+
+        if(storyManager.getStory(storyManager.getStoriesIds()[storyManager.getStoriesIds().length-1]).getPointsManagement().getPoints() != 0) {
+            System.out.println(" with these points: "+storyManager.getStory(storyManager.getStoriesIds().length).getPointsManagement().getPoints());
+        }
+
+        if(storyManager.getStory(storyManager.getStoriesIds()[storyManager.getStoriesIds().length-1]).getTitle() != null) {
+            System.out.println(" with this title: "+storyManager.getStory(storyManager.getStoriesIds().length).getTitle());
+        }
+
     }
 }
